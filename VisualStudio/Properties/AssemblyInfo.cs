44efed36789c1be154f528f9d7887574e88fc3db
Marcy <87911841+DemonBunnyBon@users.noreply.github.com>
2024-07-19 20:23:30 +0200

    Add files via upload

--- VisualStudio/Properties/AssemblyInfo.cs
+++ VisualStudio/Properties/AssemblyInfo.cs
@@ -9,9 +9,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Marcy")]
 
 
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
-[assembly: MelonInfo(typeof(PineNeedleTea), "Pine Needle Tea", "1.0.1", "Marcy", null)]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: MelonInfo(typeof(PineNeedleTea), "PineNeedleTea", "1.0.2", "Marcy", null)]
 
 
 [assembly: MelonGame("Hinterland", "TheLongDark")]
